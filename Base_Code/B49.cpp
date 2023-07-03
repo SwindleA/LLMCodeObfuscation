e88d098c343ad9bf58f644f3d280af2ade033cb5
Adrian Swindle <aswindle724@gmail.com>
2023-07-03 14:54:02 -0600

    Added base code 49

---
+++ Base_Code/B49.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double num1, num2, product;
+  cout << "Input: ";
+
+ 
+  cin >> num1 >> num2;
+ 
+  product = num1 * num2;  
+
+  cout << "Output: " << product;    
+    
+  return 0;
+}
