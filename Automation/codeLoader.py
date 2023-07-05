41e82770848085fb441fa9c1ca5fb1a1a02ff530
Adrian Swindle <aswindle724@gmail.com>
2023-07-05 09:25:04 -0600

    Header row auto format and dropdown add, complete

--- Automation/codeLoader.py
+++ Automation/codeLoader.py
@@ -10,21 +10,26 @@ from openpyxl.formula import Tokenizer
 from openpyxl.worksheet.datavalidation import DataValidation
 
 
-# def initialFormattingO(current_worksheet_o):
+def initialFormatting(current_worksheet):
 
-#     alphabet = string.ascii_uppercase
+#Creates the header row. Inserts the link to the template, fills in light blue, and freezes it
+    alphabet = string.ascii_uppercase
 
-#     fill = PatternFill(fill_type='solid', fgColor='C9DAF8')
-#     font = Font(size=15, bold=True)
+    fill = PatternFill(fill_type='solid', fgColor='C9DAF8')
+    font = Font(size=15, bold=True)
 
-#     for letter in alphabet:
-#         current_worksheet_o[letter+'1'].value = '=[ObfuscationCategorization.xlsx]Template!'+letter+'22'
-#         current_worksheet_o[letter+'1'].fill = fill
-#         current_worksheet_o[letter+'1'].font = font
+    for letter in alphabet:
+        current_worksheet[letter+'1'].value = '=[ObfuscationCategorization.xlsx]Template!'+letter+'22'
+        current_worksheet[letter+'1'].fill = fill
+        current_worksheet[letter+'1'].font = font
+
+    current_worksheet.freeze_panes = 'A2'
+
+#Creating the dropdowns
 
 #     # Define the list of values and their formatting options
-#     values = ['High Correct', 'Medium Correct', 'Low Correct','High Maybe', 'Medium Maybe','Low Maybe','Low Incorrect','Medium Incorrect', 'High Incorrect','N/A']
-#     formats = [PatternFill(fgColor='1dad00'), PatternFill(fgColor='2bff00'), PatternFill(fgColor='98fa84'), PatternFill(fgColor='fa7a02'), PatternFill(fgColor='f78b25'), PatternFill(fgColor='ff9b3d'), PatternFill(fgColor='ff6970'), PatternFill(fgColor='fc323d'), PatternFill(fgColor='db000b'),  PatternFill(fgColor='9ca3ad')]
+    values = ['High Correct', 'Medium Correct', 'Low Correct','High Maybe', 'Medium Maybe','Low Maybe','Low Incorrect','Medium Incorrect', 'High Incorrect','N/A']
+    formats = [PatternFill(fgColor='1dad00'), PatternFill(fgColor='2bff00'), PatternFill(fgColor='98fa84'), PatternFill(fgColor='fa7a02'), PatternFill(fgColor='f78b25'), PatternFill(fgColor='ff9b3d'), PatternFill(fgColor='ff6970'), PatternFill(fgColor='fc323d'), PatternFill(fgColor='db000b'),  PatternFill(fgColor='9ca3ad')]
 
 #     # Create a Tokenizer to convert the list of values into a formula
     
@@ -41,19 +46,29 @@ from openpyxl.worksheet.datavalidation import DataValidation
 
     
 
-#     data_validation = DataValidation(type="list", formula1=f'"{",".join(values)}"')
+    data_validation = DataValidation(type="list", formula1=f'"{",".join(values)}"')
 
+    current_worksheet.add_data_validation(data_validation)
+    
+    #for value, format in zip(values, formats):
+        #current_worksheet_o.conditional_formatting.add(f'{range_to_format}="{value}"', Rule(type="expression", dxfId=0, formula=[f'{list_formula}="{value}"'], stopIfTrue=True))
+    
+    #For Question 1, i.e. column I
+    for row in current_worksheet.iter_rows(min_row=2, max_row=56, min_col=9, max_col=9):
+        for cell in row:
+            
+            #cell.fill = format
+            data_validation.add(cell)
 
-#     for value, format in zip(values, formats):
-#         #current_worksheet_o.conditional_formatting.add(f'{range_to_format}="{value}"', Rule(type="expression", dxfId=0, formula=[f'{list_formula}="{value}"'], stopIfTrue=True))
-#         for row in current_worksheet_o.iter_rows(min_row=2, max_row=56, min_col=9, max_col=9):
-#             for cell in row:
-                
-#                 cell.fill = format
-#                 data_validation.add(cell)
+    #For Question 2, i.e. column U
+    for row in current_worksheet.iter_rows(min_row=2, max_row=56, min_col=21, max_col=21):
+        for cell in row:
+            
+            #cell.fill = format
+            data_validation.add(cell)
+
     
-    
-#     current_worksheet_o.freeze_panes = 'A2'
+
 
 
 
@@ -76,24 +91,24 @@ ob_code_column = 'C'
 
 for root,dirs, files in sorted(os.walk(root_dir)):
     
-#Load Base Code
-    if(root == '../ObfuscationDatabase\Base_Code'):
-        if 'List_Of_Base_Programs' not in workbook.sheetnames:
-            current_worksheet = workbook.create_sheet(title = 'List_Of_Base_Programs')
-        else:
-            current_worksheet = workbook['List_Of_Base_Programs']
+# #Load Base Code
+#     if(root == '../ObfuscationDatabase\Base_Code'):
+#         if 'List_Of_Base_Programs' not in workbook.sheetnames:
+#             current_worksheet = workbook.create_sheet(title = 'List_Of_Base_Programs')
+#         else:
+#             current_worksheet = workbook['List_Of_Base_Programs']
        
-        for file in files:
+#         for file in files:
             
-            with open(root+ '\\'+file, 'r') as temp_file:
-                file_contents = temp_file.read()
+#             with open(root+ '\\'+file, 'r') as temp_file:
+#                 file_contents = temp_file.read()
                 
-                temp_string = file[1:].split('.')
-                cell_num = str(int(temp_string[0])+1)
-                current_worksheet[name_column+cell_num].value = file.split('.')[0]
-                current_worksheet['E'+cell_num].value = file_contents
-                current_worksheet[name_column+cell_num].alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
-                current_worksheet['E'+cell_num].alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+#                 temp_string = file[1:].split('.')
+#                 cell_num = str(int(temp_string[0])+1)
+#                 current_worksheet[name_column+cell_num].value = file.split('.')[0]
+#                 current_worksheet['E'+cell_num].value = file_contents
+#                 current_worksheet[name_column+cell_num].alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+#                 current_worksheet['E'+cell_num].alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
 
 #Load Obfuscations
     if(root.split('\\')[-1][0] == 'O') :
@@ -103,7 +118,7 @@ for root,dirs, files in sorted(os.walk(root_dir)):
         if folder_name not in workbook.sheetnames:
             current_worksheet_o = workbook.create_sheet(title = folder_name)
             
-            #initialFormattingO(current_worksheet_o)
+            initialFormatting(current_worksheet_o)
 
 
 
@@ -120,17 +135,25 @@ for root,dirs, files in sorted(os.walk(root_dir)):
             with open(root+ '\\'+file, 'r', encoding='utf-8') as temp_file:
                 file_contents = temp_file.read()
                 
-                # Loading into O sheet
+    # Add obfuscation to O sheet
+
+                #Getting the base code name from the file name
                 base_code_name = file.split('.')[0].split('_')[-1]
                 
+                #setting the row number to be 1 more than the base code name
                 row_number = str(int(base_code_name[1:])+1)
                 
+                #Cell with Obfuscation name
                 name_cell = current_worksheet_o[name_column+row_number]
+
+                #Cell with obfuscated code
                 o_code_cell = current_worksheet_o[ob_code_column+row_number]
 
-
+                #Adding the Data to the cells
                 name_cell.value = base_code_name
                 o_code_cell.value = file_contents
+
+                #Cell formatting
                 name_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
                 o_code_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
                 
@@ -141,18 +164,8 @@ for root,dirs, files in sorted(os.walk(root_dir)):
                 if base_code_name not in workbook.sheetnames:
                     current_worksheet_b = workbook.create_sheet(title = base_code_name)
 
-                    alphabet = string.ascii_uppercase
+                    initialFormatting(current_worksheet_b)
 
-                    fill = PatternFill(fill_type='solid', fgColor='C9DAF8')
-                    font = Font(size=15, bold=True)
-
-                    for letter in alphabet:
-                        current_worksheet_b[letter+'1'].value = '=[ObfuscationCategorization.xlsx]Template!'+letter+'1'
-                        current_worksheet_b[letter+'1'].fill = fill
-                        current_worksheet_b[letter+'1'].font = font
-
-
-                    current_worksheet_b.freeze_panes = 'A2'
 
                 else:
                     current_worksheet_b = workbook[base_code_name]
@@ -161,14 +174,17 @@ for root,dirs, files in sorted(os.walk(root_dir)):
                 #setting the row number to be 1 more than the obfuscation name
                 row_number = str(int(folder_name[1:])+1)
                 
+                #Cell where the name of the base code name is
                 name_cell = current_worksheet_b[name_column+row_number]
+
+                #Cell where the base code is
                 b_code_cell = current_worksheet_b[ob_code_column+row_number]
                 
                 #Adding the Data to the cells
                 name_cell.value = folder_name
                 b_code_cell.value = file_contents
 
-                #Formatting
+                #Formatting cells
                 name_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
                 b_code_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
 
