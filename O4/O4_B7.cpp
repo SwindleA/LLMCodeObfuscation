657e6f1c6d02476814f81f830f0c3e054be3e8f3
Adrian Swindle <aswindle724@gmail.com>
2023-07-06 13:32:48 -0600

    fixed O4_B7

--- O4/O4_B7.cpp
+++ O4/O4_B7.cpp
@@ -1,1 +1,39 @@
-/** N/A **/
+#include <iostream>
+
+int Monday(int n) {
+    if (n <= 0) return 1;
+    else return n * Monday(n - 1);
+}
+
+double Wednesday(double n) {
+    if (n <= 0.1) return n;
+    else return n * Wednesday(n - 0.1);
+}
+
+char Friday(char n) {
+    if (n <= 'a') return n;
+    else return Friday(n - 1);
+}
+
+int main() {
+    std::string firstHalf = "Hello";
+    std::string secondHalf = "There";
+
+    int firstHalfNumberOfTimes = 4;
+    int secondHalfNumberOfTimes = 7;
+
+    std::string fullWord;
+
+    for (int i = 0; i < firstHalfNumberOfTimes; i++) {
+        fullWord.append(firstHalf);
+    }
+    for (int j = 0; j < secondHalfNumberOfTimes; j++) {
+        fullWord.append(secondHalf);
+    }
+   
+    int Sunday = Monday(3);
+    double Tuesday = Wednesday(2.5);
+    char Thursday = Friday('c');
+
+    std::cout << fullWord << std::endl;
+}
