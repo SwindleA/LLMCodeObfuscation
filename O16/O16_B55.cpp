4cfc12f8f0413bbd0e0aec87a31205e4f3f246f0
Adrian Swindle <aswindle724@gmail.com>
2023-07-06 15:54:52 -0600

    fixing some obfuscations

--- O16/O16_B55.cpp
+++ O16/O16_B55.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main()
-{
-    string output;
-
-    
-    output = "123";
-
-    cout << output;
-
-    return 0;
-}
+/** N/A **/
