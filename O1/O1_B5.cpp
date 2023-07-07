cc834f92b286f879f737c85ff7e99aba89bc22fa
Adrian Swindle <aswindle724@gmail.com>
2023-07-07 11:42:06 -0600

    Updated 01 b5

--- O1/O1_B5.cpp
+++ O1/O1_B5.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include <vector>
+
 
 int main(){
     
-    std::vector<int> number;
-    int i =10;
+    std::string secondHalf = "race";
+    std::string firstHalf = "car";
 
-    for(int factors =i; factors>0;factors--){
-        if(i %factors == 0){
-            number.push_back(factors);
-        }
-        
-    }
+    std::string fullWord = secondHalf.append(firstHalf);
 
-    for(int j = 0; j< number.size(); j++){
-        std::cout<< number.at(j)<< ' ';
-    }
+    std::cout<<fullWord<<std::endl;
+
     
-
 }
