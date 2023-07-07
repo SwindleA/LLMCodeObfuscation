4eca39ac222cf6511b1becb3f156907df8766a35
Adrian Swindle <aswindle724@gmail.com>
2023-07-07 16:31:08 -0600

    workingon q2 formatting

--- Automation/chatGPT.py
+++ Automation/chatGPT.py
@@ -33,3 +33,33 @@ def askQuestion(question):
     
     
     return response['choices'][0]['message']['content']
+
+def yesnoQuestion(question,follow_up_question):
+    
+    while True:
+
+        try:
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content" : question},{"role":"assistant","content": "Yes or No \n"},{"role": "user", "content" : follow_up_question}],
+                temperature=0.7,
+                max_tokens=2048,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+                )
+            break
+        except openai.error.ServiceUnavailableError as e:
+            print("API error:", e)
+            print("Retrying in 10 seconds...")
+            time.sleep(10)
+        except Exception as ex:
+            print("API exception:", ex)
+            print("Retrying in 10 seconds...")
+            time.sleep(10)
+
+    
+    print(response['choices'][0]['message'])
+    return response['choices'][0]['message']['content']
+
--- Automation/questionLoader_Q2.py
+++ Automation/questionLoader_Q2.py
@@ -2,14 +2,24 @@
 import os
 import openpyxl
 from openpyxl.styles import Font, Alignment
+import time
 
+import chatGPT
+import jurassic_2
+import palm
 
 root_dir_workbook = 'C:\\Users\\aswin\\OneDrive - Saint Louis University\\Documents\\REU-2023\\REU-Project\\ObfuscationDatabase'
 root_dir = '..'
 
 # Load the Excel spreadsheet
-current_workbook = '\\LM2.xlsx'#'\\ObfuscationCategorization.xlsx'
-workbook = openpyxl.load_workbook(root_dir_workbook+current_workbook)
+current_workbook = '\\Q2Test.xlsx'#'\\PaLM_Q1.xlsx'#'\\ChatGPT_Q1.xlsx'##'\\ObfuscationCategorization.xlsx''\\LM2.xlsx'
+
+#Change this depending on the Language Model being used
+LM = chatGPT
+
+follow_up_question = 'explain'
+
+workbook = openpyxl.load_workbook(root_dir_workbook+ current_workbook)
 
 question_number = 'Q2'
 
@@ -24,7 +34,8 @@ code_two = 8
 
 with open('Question_Templates\\'+question_number+'.txt', 'r', encoding='utf-8') as question_template:
     question = question_template.readlines()
-    
+    print(question_template.read())
+
 
 
 
@@ -34,6 +45,9 @@ for root,dirs, files in sorted(os.walk(root_dir)):
     if(root.split('\\')[-1][0] == 'O') :
         folder_name = root.split('\\')[-1]
         print("Folder name: ",folder_name)
+        
+        
+
         current_worksheet_o = workbook[folder_name]
 
         #for each file in the folder
@@ -52,6 +66,7 @@ for root,dirs, files in sorted(os.walk(root_dir)):
                     continue
 
             base_code_name = file.split('.')[0].split('_')[-1]
+            
             row_number_int = int(base_code_name[1:])+1
 
             if(row_number_int%2 == 0):
@@ -73,42 +88,79 @@ for root,dirs, files in sorted(os.walk(root_dir)):
             # with open(root_dir+base_code_name+".txt", 'w',encoding='utf-8') as file:
             #     file.writelines(temp_question)
 
-            # Loading question into O sheet
+
+
+# Loading question and answer into O sheet
             
+            #getting answer to question
+            answer = LM.yesnoQuestion(''.join(temp_question),follow_up_question)
             
+
+            if(LM == palm and answer == 'NONE'):
+                print(folder_name + base_code_name)
+
             o_row_number = str(int(base_code_name[1:])+1)
             o_1_question_cell = current_worksheet_o[_1_question_column+o_row_number]
             o_2_question_cell = current_worksheet_o[_2_question_column+o_row_number]
             
-            #initial question
+            o_1_answer_cell = current_worksheet_o[_1_answer_column+o_row_number]
+            o_2_answer_cell = current_worksheet_o[_2_answer_column+o_row_number]
+
+            #initial question and answer loading into o sheet
             o_1_question_cell.value = ''.join(temp_question)
+            o_1_answer_cell.value = ''.join(answer.split('\n')[0])
 
             #follow up question
-            o_2_question_cell.value = 'explain'
+            o_2_question_cell.value = follow_up_question
+            o_2_answer_cell.value = ''.join(answer.split('\n')[1:])
 
-            #formatting
+            #formatting for o sheet
             o_1_question_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+            o_1_answer_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+
             o_2_question_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+            o_2_answer_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
             
 
-            # Loading question into B sheet
+            # Loading question and answer into B sheet
             current_worksheet_b = workbook[base_code_name]
 
             b_row_number = str(int(folder_name[1:])+1)
             b_1_question_cell = current_worksheet_b[_1_question_column+b_row_number]
-            b_2_question_cell = current_worksheet_b[_2_question_column+b_row_number]
+            b_1_answer_cell = current_worksheet_b[_1_answer_column+b_row_number]
 
-            #initial question
+            b_2_question_cell = current_worksheet_b[_2_question_column+b_row_number]
+            b_2_answer_cell = current_worksheet_b[_2_answer_column+b_row_number]
+
+            
+            #initial question and answer loading into o sheet
             b_1_question_cell.value = ''.join(temp_question)
+            b_1_answer_cell.value = ''.join(answer.split('\n')[0])
 
             #follow up question
-            b_2_question_cell.value = 'explain'
+            b_2_question_cell.value = follow_up_question
+            b_2_answer_cell.value = ''.join(answer.split('\n')[1:])
 
+           
             #formatting
             b_1_question_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+            b_1_answer_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+
             b_2_question_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
+            b_2_answer_cell.alignment = Alignment(wrapText=True,horizontal='left', vertical='top')
             
-                
+            if(LM == chatGPT):
+                start_time = time.perf_counter()
+                while time.perf_counter() - start_time < 21:
+                    pass
+
+            # Save the changes
+            workbook.save(root_dir_workbook+current_workbook)
+            
+            if(base_code_name == "B4"):
+                print("code contains break statement")
+                break
+        break     
         
 
 
